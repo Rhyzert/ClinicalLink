a019990aa2cd3400311320c575417b59c1393895
Rhyzert <leoflorentino97@gmail.com>
2024-09-10 16:27:40 -0300

    main

---
+++ ClinicalLink/ClinicalLink.Infrastructure/DbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicalLink.Infrastructure
+{
+    public class SqlContextFactory : IDesignTimeDbContextFactory<SqlContext>
+    {
+        public SqlContext CreateDbContext(string[] args)
+        {
+            var path = @"D:\Projeto\ClinicalLink\ClinicalLink\ClinicalLink";
+            // Cria uma configuração para acessar a string de conexão do appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<SqlContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new SqlContext(optionsBuilder.Options);
+        }
+    }
+}
